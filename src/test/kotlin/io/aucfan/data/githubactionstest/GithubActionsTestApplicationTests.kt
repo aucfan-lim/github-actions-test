2d2196aa80869b728d8953991e3ede429dbd77ef
dongoh.lim <dongoh.lim@aucfan.com>
2025-11-11 07:52:49 +0900

    add func

---
+++ src/test/kotlin/io/aucfan/data/githubactionstest/GithubActionsTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.aucfan.data.githubactionstest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubActionsTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
